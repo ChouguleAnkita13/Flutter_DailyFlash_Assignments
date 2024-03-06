fe61d2cf81456073147caede5dbc1c1ce10b4e13
ChouguleAnkita13 <flutterankita@gmail.com>
2024-03-06 20:47:00 +0530

    task2 modified

--- DailyFlash-03/dailyflash_03/lib/task2.dart
+++ DailyFlash-03/dailyflash_03/lib/task2.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'task3.dart';
-class Task2 extends StatelessWidget{
+
+class Task2 extends StatelessWidget {
   const Task2({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      // body: Center(child: 
-      // Container(
-      //   height: 300,
-      //   width: 300,
-      //   color: Colors.blueGrey,
-      //   child: Image.asset(""),
-      // )),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const Task3()));
-      },
-      child: const Icon(Icons.forward),),
+      body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            alignment: Alignment.center,
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage("assets/image.png",),fit: BoxFit.fill)),
+        child: const Text("Ankita"),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => const Task3()));
+        },
+        child: const Icon(Icons.forward),
+      ),
     );
   }
 }
